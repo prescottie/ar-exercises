4cd998dec3f1042fba46db20d5139aa7d514594a
Scotty <scott.hagan@aol.com>
2017-10-10 21:03:57 +0000

    completed exercise 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,15 +9,15 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
-@store1.employees.create([{first_name: "Scotty", last_name: "Hagan", hourly_rate: 2},
-                          {first_name: "Spencer", last_name: "mamilton", hourly_rate: 900},
-                          {first_name: "Jenny", last_name: "JLo", hourly_rate: 313},
-                          {first_name: "Tasha", last_name: "URBZ", hourly_rate: 394}])
+@store1.employees.create([{first_name: "Scotty", last_name: "Hagan", hourly_rate: 41},
+                          {first_name: "Spencer", last_name: "mamilton", hourly_rate: 41},
+                          {first_name: "Jenny", last_name: "JLo", hourly_rate: 41},
+                          {first_name: "Tasha", last_name: "URBZ", hourly_rate: 41}])
 
-@store2.employees.create([{first_name: "Isaac", last_name: "V", hourly_rate: 23},
-                          {first_name: "LL", last_name: "Cool J", hourly_rate: 100000},
-                          {first_name: "Joel", last_name: "Shinness", hourly_rate: -20}])
+@store2.employees.create([{first_name: "Isaac", last_name: "V", hourly_rate: 41},
+                          {first_name: "LL", last_name: "Cool J", hourly_rate: 41},
+                          {first_name: "Joel", last_name: "Shinness", hourly_rate: 41}])
 
 
-puts "#{@store1.name} has #{@store1.employees.count} emlpoyees"
-puts "#{@store2.name} has #{@store2.employees.count} emlpoyees"
+puts "#{@store1.name} has #{@store1.employees.count} employees"
+puts "#{@store2.name} has #{@store2.employees.count} employees"
