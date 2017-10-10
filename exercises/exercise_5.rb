35e1fb185638a631c987c76cd791fdd13a309fa9
Scotty <scott.hagan@aol.com>
2017-10-10 19:38:03 +0000

    completed exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,15 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+all_stores = Store.all
+@all_stores_rev = all_stores.sum(:annual_revenue)
+@all_stores_rev_avg = (all_stores.sum(:annual_revenue)/ all_stores.count)
+@all_1M_stores = all_stores.where('annual_revenue > 1000000').count
+
+puts "Total revenue for all stores is $#{@all_stores_rev}"
+puts "Average revenue for all stores is $#{@all_stores_rev_avg}"
+
+puts "#{@all_1M_stores} Stores generate over 1 Mil in sales"
+# @count_1M_stores = @all_1M_stores.count
+# puts @all_1M_stores
+
