e809c6384ccc68da8aeb1b1547c4fbc642a9ac4b
Scotty <scott.hagan@aol.com>
2017-10-10 19:10:33 +0000

    completed exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,12 @@
 require_relative '../setup'
 
+
 puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+burnab = Store.create name:"burnabi", annual_revenue:300000, mens_apparel: true, womens_apparel: true
+rich = Store.create name:"RichCity", annual_revenue:1260000, mens_apparel: false, womens_apparel: true
+gas = Store.create name:"GasTown", annual_revenue:190000, mens_apparel: true, womens_apparel: false
+
+puts Store.all.count 
